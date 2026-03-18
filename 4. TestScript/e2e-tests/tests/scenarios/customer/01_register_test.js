71a2a55b65430ee6905bf1314681495092e3f73c
Hồ Nguyễn Phúc Thông <phucthong0412@gmail.com>
2026-03-19 00:41:17 +0700

    a

--- 4. TestScript/e2e-tests/tests/scenarios/customer/01_register_test.js
+++ 4. TestScript/e2e-tests/tests/scenarios/customer/01_register_test.js
@@ -1,20 +1,6 @@
 const dataGenerator = require("../../helpers/dataGenerator");
-const testData = require("../../data/users.json");
 
 Feature("Customer Registration - Đăng ký tài khoản");
-
-const validationMessages = {
-  duplicateEmail: "Email đã được sử dụng",
-  invalidEmail: "Email không hợp lệ",
-  invalidPhone: "Số điện thoại không hợp lệ",
-  weakPassword: "Mật khẩu phải có ít nhất 6 ký tự",
-  passwordMismatch: "Mật khẩu không khớp!",
-  requiredName: "Nhập họ tên!",
-  requiredEmail: "Nhập email!",
-  requiredPhone: "Nhập SĐT!",
-  requiredPassword: "Nhập mật khẩu!",
-  requiredConfirmPassword: "Xác Nhận mật khẩu!",
-};
 
 Before(({ I }) => {
   I.amOnPage("/");
@@ -22,32 +8,27 @@ Before(({ I }) => {
   I.clearCookie();
 });
 
-Scenario("TC_AUTH_009: Hiển thị form đăng ký", ({ I, registerPage }) => {
+Scenario("TC_REG_009: Hiển thị form đăng ký", ({ I, registerPage }) => {
   registerPage.open();
   registerPage.seeRegisterForm();
   I.see("Đăng Ký");
+  I.saveScreenshot("TC_REG_001_register_form.png");
 });
 
 Scenario(
-  "TC_AUTH_010: Đăng ký thành công với thông tin hợp lệ",
+  "TC_REG_010: Đăng ký thành công với thông tin hợp lệ",
   ({ I, registerPage }) => {
     const user = dataGenerator.generateUser();
-
     registerPage.open();
-    registerPage.register(
-      user.fullName,
-      user.email,
-      user.phone,
-      user.password,
-      user.password,
-    );
+    registerPage.register(user.fullName, user.email, user.phone, user.password);
     I.wait(5);
-    I.dontSee(validationMessages.duplicateEmail);
+    I.saveScreenshot("TC_REG_002_register_success.png");
   },
 );
 
-Scenario("TC_AUTH_011: Đăng ký với email đã tồn tại", ({ I, registerPage }) => {
-  const existingUser = testData.customer.seed;
+Scenario("TC_REG_011: Đăng ký với email đã tồn tại", ({ I, registerPage }) => {
+  const testData = require("../../data/users.json");
+  const existingUser = testData.customer.valid;
   const user = dataGenerator.generateUser();
 
   registerPage.open();
@@ -56,106 +37,85 @@ Scenario("TC_AUTH_011: Đăng ký với email đã tồn tại", ({ I, registerP
     existingUser.email,
     user.phone,
     user.password,
-    user.password,
   );
-  I.wait(5);
-  I.see(validationMessages.duplicateEmail);
+  I.wait(3);
+  I.saveScreenshot("TC_REG_003_duplicate_email.png");
 });
 
 Scenario(
-  "TC_AUTH_012: Đăng ký với email không hợp lệ",
+  "TC_REG_012: Đăng ký với các trường bắt buộc để trống",
+  ({ I, registerPage }) => {
+    registerPage.open();
+    registerPage.clickSubmit();
+    I.wait(2);
+    I.saveScreenshot("TC_REG_004_empty_fields.png");
+  },
+);
+
+Scenario(
+  "TC_REG_013: Đăng ký với email không hợp lệ",
   ({ I, registerPage }) => {
     const user = dataGenerator.generateUser();
-
     registerPage.open();
     registerPage.register(
       user.fullName,
       "invalid-email",
       user.phone,
       user.password,
-      user.password,
     );
     I.wait(2);
-    I.see(validationMessages.invalidEmail);
+    I.saveScreenshot("TC_REG_005_invalid_email.png");
   },
 );
 
-Scenario(
-  "TC_AUTH_013: Đăng ký với SĐT không hợp lệ (9 số)",
-  ({ I, registerPage }) => {
-    const user = dataGenerator.generateUser();
-
-    registerPage.open();
-    registerPage.register(
-      user.fullName,
-      user.email,
-      "091234567",
-      user.password,
-      user.password,
-    );
-    I.wait(2);
-    I.see(validationMessages.invalidPhone);
-  },
-);
-
-Scenario("TC_AUTH_014: Đăng ký với mật khẩu yếu", ({ I, registerPage }) => {
+Scenario("TC_REG_014: Đăng ký với SĐT không hợp lệ", ({ I, registerPage }) => {
   const user = dataGenerator.generateUser();
-
   registerPage.open();
-  registerPage.register(user.fullName, user.email, user.phone, "123", "123");
+  registerPage.register(user.fullName, user.email, "123", user.password);
   I.wait(2);
-  I.see(validationMessages.weakPassword);
+  I.saveScreenshot("TC_REG_006_invalid_phone.png");
+});
+
+Scenario("TC_REG_015: Đăng ký với mật khẩu yếu", ({ I, registerPage }) => {
+  const user = dataGenerator.generateUser();
+  registerPage.open();
+  registerPage.register(user.fullName, user.email, user.phone, "123");
+  I.wait(2);
+  I.saveScreenshot("TC_REG_007_weak_password.png");
 });
 
 Scenario(
-  "TC_AUTH_015: Đăng ký với xác nhận mật khẩu không khớp",
+  "TC_REG_016: Đăng ký với mật khẩu xác nhận không khớp",
   ({ I, registerPage }) => {
     const user = dataGenerator.generateUser();
-
     registerPage.open();
-    registerPage.register(
-      user.fullName,
-      user.email,
-      user.phone,
-      "Test@1234",
-      "DifferentPass@123",
-    );
-    I.wait(2);
-    I.see(validationMessages.passwordMismatch);
-  },
-);
-
-Scenario(
-  "TC_AUTH_016: Đăng ký với tất cả trường trống",
-  ({ I, registerPage }) => {
-    registerPage.open();
+    registerPage.fillName(user.fullName);
+    registerPage.fillEmail(user.email);
+    registerPage.fillPhone(user.phone);
+    registerPage.fillPassword(user.password);
+    registerPage.fillConfirmPassword("DifferentPassword@123");
     registerPage.clickSubmit();
     I.wait(2);
-    I.see(validationMessages.requiredName);
-    I.see(validationMessages.requiredEmail);
-    I.see(validationMessages.requiredPhone);
-    I.see(validationMessages.requiredPassword);
-    I.see(validationMessages.requiredConfirmPassword);
+    I.saveScreenshot("TC_REG_008_password_mismatch.png");
   },
 );
 
 Scenario(
-  "TC_AUTH_017: Chuyển từ form đăng nhập sang đăng ký",
-  ({ I, loginPage }) => {
-    loginPage.open();
-    loginPage.clickRegisterLink();
-    I.wait(2);
-    I.seeInCurrentUrl("/register");
-    I.see("Đăng ký");
+  "TC_REG_017: Chuyển sang trang đăng nhập từ form đăng ký",
+  ({ I, registerPage }) => {
+    registerPage.open();
+    registerPage.clickLoginLink();
+    I.wait(3);
+    I.seeInCurrentUrl("/login");
+    I.saveScreenshot("TC_REG_009_navigate_login.png");
   },
 );
-
 Scenario(
   "TC_AUTH_018: Chuyển từ form đăng ký sang đăng nhập",
   ({ I, registerPage }) => {
     registerPage.open();
     registerPage.clickLoginLink();
-    I.wait(2);
+    I.wait(5);
     I.seeInCurrentUrl("/login");
     I.see("Đăng nhập");
   },
